c63cac50114d57d62f48c2dc33282d9ac83c10ba
Jorge Luiz <jorgeluiz1586@gmail.com>
2022-06-26 17:24:00 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import "@/assets/base.css";
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  list-style: none;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+</style>
